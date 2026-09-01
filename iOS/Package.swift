6501c71fb324c01b673130998d9fc577b6182d4c
Atharv Dwivedi <atharvdwivedi201311@gmail.com>
2026-09-01 16:48:50 +0530

    Refactor Package.swift for cleaner structure

--- iOS/Package.swift
+++ iOS/Package.swift
@@ -7,21 +7,15 @@ let package = Package(
         .iOS(.v18)
     ],
     products: [
-        // EXPORTED OUTPUT: Exposes your code as an executable app or library
         .executable(
             name: "PortalInvasionApp",
             targets: ["PortalInvasionApp"]
         )
     ],
-    dependencies: [
-        // Add third-party Swift package dependencies here
-    ],
     targets: [
-        // INTERNAL MODULE: Compiles your Swift source code files
         .executableTarget(
             name: "PortalInvasionApp",
-            dependencies: [],
-            path: "." // Tells SPM to read source files in the current folder (Views, Game, etc.)
+            path: "."
         )
     ]
 )
